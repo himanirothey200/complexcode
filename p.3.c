25d746a88490460ccfe428cd534be510d263dfcb
Himani Suresh Rothey <himanirothey200@gmail.com>
2023-03-16 09:23:33 -0400

    program3

---
+++ p.3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void Accept(int iNo) 
+{
+    int iCnt = 0;
+    for(iCnt = 1;iCnt<=1;iCnt++)
+    {
+        printf("*\n");
+    }
+}
+
+
+int main()
+{
+    Display();
+    return 0;
+}
