0a15216941ed110db6e3a973adb818dd0ded746a
Himani Suresh Rothey <himanirothey200@gmail.com>
2023-03-16 09:23:24 -0400

    program2

---
+++ p.2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void Display()
+{
+    int iNo = 0;
+    for(iNo = 1;iNo<=5;iNo++)
+    {
+        printf("Marvellous\n");
+    }
+}
+
+
+int main()
+{
+    Display();
+    return 0;
+}
