5ad68dec84ffbea1f10ee5251adec7153da322cc
Himani Suresh Rothey <himanirothey200@gmail.com>
2023-03-16 09:26:11 -0400

    program8

---
+++ p.8.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    while(iNo>0)
+    {
+        printf("*");
+        iNo--;
+    }
+}
+
+int main()
+{
+    int iValue=0;
+    printf("Enter the value :");
+    scanf("%d",&iValue);
+    Display(iValue);
+    return 0;
+}
